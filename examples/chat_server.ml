6765622bddd720deb6514aa5367cf8d901e31667
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-28 06:21:54 -0700

    Seemingly working

--- examples/chat_server.ml
+++ examples/chat_server.ml
@@ -1,52 +1,48 @@
 (* Basically a translation of
    http://arminboss.de/2013/tutorial-how-to-create-a-basic-chat-with-node-js/ *)
-open Nodejs_kit
 
-let program () =
-  let http = Nodejs.Http.require () in
-  let fs = Nodejs.Fs.require () in
-  let io = Socket_io.require () in
-
-  let port = 8080 in
-  let headers = js_object_of_alist [("Content-Type", "text/html")] in
-  let server =
-    http##createServer
-      !@ begin
-      fun request response ->
-        fs##readFile !$"./client.html"
-          !@ (fun error raw_data ->
-              response##writeHead 200 headers;
-              response##end_data raw_data)
-    end
-  in
-  let app = server##listen port
-      !@ begin fun () ->
-      Printf.sprintf
-        "\n\nStarted Server on local host port %d, node version: %s"
-        port
-        Nodejs.Process.version
-      |> print_endline
-    end
-  in
-  let io = io##listen app in
-  (* Gives back a namespace object *)
-  io##.sockets##on
-    !$ "connection"
-    (* And now we get a socket *)
-    !@ begin fun socket ->
-    socket##on
-      !$ "message_to_server"
-      (* For which we have some data, its an object *)
-      !@ begin fun data ->
-      let innard = Js.Unsafe.get data "message" in
-      io##.sockets##emit
-        !$"message_to_client"
-        (js_object_of_alist [("message", innard)])
-    end
-  end
-
-let run p =
-  ignore (p ())
+(*   let headers = js_object_of_alist [("Content-Type", "text/html")] in *)
+(*   let server = *)
+(*     http##createServer *)
+(*       !@ begin *)
+(*       fun request response -> *)
+(*         (\* fs##readFile !$"./client.html" *\) *)
+(*         (\*   !@ (fun error raw_data -> *\) *)
+(*         (\*       response##writeHead 200 headers; *\) *)
+(*         (\*       response##end_data raw_data) *\) *)
+(*         () *)
+(*     end *)
+(*   in *)
+(*   let app = server##listen port *)
+(*       !@ begin fun () -> *)
+(*       Printf.sprintf *)
+(*         "\n\nStarted Server on local host port %d, node version: %s" *)
+(*         port *)
+(*         Nodejs.Process.version *)
+(*       |> print_endline *)
+(*     end *)
+(*   in *)
+(*   let io = io##listen app in *)
+(*   (\* Gives back a namespace object *\) *)
+(*   io##.sockets##on *)
+(*     !$ "connection" *)
+(*     (\* And now we get a socket *\) *)
+(*     !@ begin fun socket -> *)
+(*     socket##on *)
+(*       !$ "message_to_server" *)
+(*       (\* For which we have some data, its an object *\) *)
+(*       !@ begin fun data -> *)
+(*       let innard = Js.Unsafe.get data "message" in *)
+(*       io##.sockets##emit *)
+(*         !$"message_to_client" *)
+(*         (js_object_of_alist [("message", innard)]) *)
 
 let () =
-  run program
+  let module H = (val Nodejs.require Nodejs.Http) in
+  let our_server =
+    H.create_server begin fun _ _ ->
+      print_endline "hello"
+    end
+  in
+
+  our_server#listen 8080 (fun () -> print_endline "did this work")
--- src/nodejs.ml
+++ src/nodejs.ml
@@ -1,23 +1,76 @@
+let require_module s =
+  Js.Unsafe.fun_call
+    (Js.Unsafe.js_expr "require")
+    [|Js.Unsafe.inject (Js.string s)|]
+
+(** Same as console.log *)
+let log obj = Firebug.console##log obj
+
+(** The current file name *)
+let __filename () =
+  (Js.Unsafe.eval_string "__filename" : Js.js_string Js.t) |> Js.to_string
+
+(** The current directory name *)
+let __dirname () =
+  (Js.Unsafe.eval_string "__dirname" : Js.js_string Js.t) |> Js.to_string
+
+module type Http = sig
+
+  type methods = Checkout | Connect | Copy | Delete | Get | Head | Lock |
+                 M_search | Merge | Mkactivity | Mkcalendar | Mkcol |
+                 Move | Notify | Options | Patch | Post | Propfind |
+                 Proppath | Purge | Put | Report | Search | Subscribe |
+                 Trace | Unlock | Unsubscribe
+
+  class type incoming_message = object  end
+
+  class type server_response = object end
+
+  class type server = object
+    method listen : port:int -> callback:(unit -> unit) -> unit
+  end
+
+  val create_server : (incoming_message -> server_response -> unit) -> server
+
+end
+
 module Http = struct
-  include Http
+
+  type methods = Checkout | Connect | Copy | Delete | Get | Head | Lock |
+                 M_search | Merge | Mkactivity | Mkcalendar | Mkcol |
+                 Move | Notify | Options | Patch | Post | Propfind |
+                 Proppath | Purge | Put | Report | Search | Subscribe |
+                 Trace | Unlock | Unsubscribe
+
+  module Raw_js = struct
+
+    class type http = object end
+    class type server = object end
+
+  end
+
+  class incoming_message = object end
+  class server_response = object end
+
+  class server handler = object(self)
+
+    val raw_js_server : Raw_js.server Js.t =
+      let h : Raw_js.http Js.t = require_module "http" in
+      Js.Unsafe.meth_call h "createServer" [|Js.Unsafe.inject handler|]
+
+    method listen (port:int) (handler : (unit -> unit)) : unit =
+      Js.Unsafe.meth_call raw_js_server "listen" [|Js.Unsafe.inject port;
+                                                   Js.Unsafe.inject handler|]
+
+  end
+
+  let create_server handler =
+    new server handler
+
 end
 
-module Net = struct
-  include Net
-end
+type modules = Http
 
-module Url = struct
-  include Url
-end
-
-module Events = struct
-  include Events
-end
-
-module Process = struct
-  include Process
-end
-
-module Fs = struct
-  include Fs
-end
+let require module_ =
+  match module_ with
+  | Http -> (module Http : Http)
