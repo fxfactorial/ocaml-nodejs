034990ce59b8779b92979b73abf7250eedbb2456
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-31 14:46:17 -0700

    Try to make crucuial commit call example work

--- examples/chat_server.ml
+++ examples/chat_server.ml
@@ -1,48 +1,36 @@
 (* Basically a translation of
    http://arminboss.de/2013/tutorial-how-to-create-a-basic-chat-with-node-js/ *)
 
-(*   let io = io##listen app in *)
-(*   (\* Gives back a namespace object *\) *)
-(*   io##.sockets##on *)
-(*     !$ "connection" *)
-(*     (\* And now we get a socket *\) *)
-(*     !@ begin fun socket -> *)
-(*     socket##on *)
-(*       !$ "message_to_server" *)
-(*       (\* For which we have some data, its an object *\) *)
-(*       !@ begin fun data -> *)
-(*       let innard = Js.Unsafe.get data "message" in *)
-(*       io##.sockets##emit *)
-(*         !$"message_to_client" *)
-(*         (js_object_of_alist [("message", innard)]) *)
-
 let () =
   let module H = (val Nodejs.require Nodejs.Http) in
   let module Fs = (val Nodejs.require Nodejs.Fs) in
-  let s = Socket_io.require () in
-  let our_server =
+  let io = Socket_io.require () in
+  let server =
     H.create_server begin fun incoming response ->
-      print_endline "Got a request";
 
       Fs.read_file "./client.html" None begin fun err data ->
         response#write_head 200 [("Content-type", "text/html")];
         response#end_ ~data:(H.String data) ()
-      end;
 
+      end;
     end
   in
-  let simple_server = our_server#listen ~port:8080 begin fun () ->
+  let app = server#listen ~port:8080 begin fun () ->
       Printf.sprintf
         "Started Server and Running node: %s" (new Nodejs.process#version)
       |> print_endline
     end
   in
 
-  let s2 = s##listen simple_server in
-  let sks = s2##.sockets in
-  (* Nodejs.log s; *)
-  Nodejs.log sks;
-  (* print_endline sks#name; *)
-  (* Nodejs.log sks; *)
+  let io = io#listen app in
+  io#sockets#on_connection begin fun socket ->
 
-  ()
+    socket#on "message_to_server" begin fun data ->
+      let innard = Nodejs.g data "message" in
+
+      io#sockets#emit
+        "message_to_client"
+        (Nodejs.i (object%js val message = Nodejs.i innard end))
+
+    end
+  end
