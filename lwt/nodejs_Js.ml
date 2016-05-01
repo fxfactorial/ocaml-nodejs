32626fe3a198e78c4ade9e98925bc2a563d3cb14
Michael Grünewald <michipili@gmail.com>
2016-05-01 07:23:50 +0200

    Prepare a micro lwt-ish set of functions for NodeJS

---
+++ lwt/nodejs_Js.ml
@@ -0,0 +1,54 @@
+open Js
+
+exception Error of error t
+
+let _debug =
+  ref true
+
+let debug location fmt =
+  let open Printf in
+  ksprintf
+    (fun s -> if !_debug then eprintf "Debug: %s: %s\n%!" location s)
+    fmt
+
+let require_module s =
+  Unsafe.fun_call
+    (Unsafe.js_expr "require")
+    [|Unsafe.inject (string s)|]
+
+let unsafe_string s =
+  Unsafe.inject (string s)
+
+let unsafe_callback f =
+  Unsafe.inject (Unsafe.callback f)
+
+let array_to_js : ('a -> 'b) -> 'a array -> 'b js_array t =
+  fun convert b ->
+    let a = new%js array_empty in
+    for i = 0 to Array.length b - 1 do
+      ignore(a##push (convert b.(i)));
+    done;
+    a
+
+let array_of_js : ('a -> 'b) -> 'a js_array t -> 'b array =
+  fun convert a ->
+    Array.init
+      (a##.length)
+      (fun i -> convert (Optdef.get (array_get a i) (fun () -> invalid_arg "array_get")))
+
+
+let list_of_js convert a =
+  Array.to_list (array_of_js convert a)
+
+let list_to_js convert b =
+  array_to_js convert (Array.of_list b)
+
+let stringify : 'a -> string =
+  fun s -> (to_string(Json.output s))
+
+let keys : 'a t -> string list =
+  fun obj ->
+    Unsafe.fun_call
+      (Unsafe.js_expr "Object.keys")
+      [|Unsafe.inject obj|]
+    |> list_of_js to_string
---
+++ lwt/nodejs_Js.mli
@@ -0,0 +1,56 @@
+(** Utility functions to interact with the JavaScript layer. *)
+
+
+(** {6 JavaScript errors} *)
+
+exception Error of Js.error Js.t
+(** Exception holding a JavaScript error. *)
+
+
+(** {6 JavaScript modules} *)
+
+val require_module : string -> 'a
+(** [require_module name] is the value returned by the JavaScript code
+    {i require(name)}.  The result is compatible with all types and
+    is usually constrained by a class type when used in a program. *)
+
+
+(** {6 JavaScript conversions} *)
+
+val unsafe_string : string -> Js.Unsafe.any
+(** Convert an OCaml string to a JavaScript value. *)
+
+val unsafe_callback : 'a -> 'b -> Js.Unsafe.any
+(** Convert an OCaml function to a JavaScript value. *)
+
+val array_to_js : ('a -> 'b) -> 'a array -> 'b Js.js_array Js.t
+(** [array_to_js conv a] convert the OCaml array [a] to a JavaScript
+    array, using the provided function [conv] to convert array items. *)
+
+val array_of_js : ('a -> 'b) -> 'a Js.js_array Js.t -> 'b array
+(** [array_of_js conv a] convert the JavaScript array [a] to an OCaml
+    array, using the provided function [conv] to convert array items. *)
+
+val list_to_js : ('a -> 'b) -> 'a list -> 'b Js.js_array Js.t
+(** [array_to_js conv a] convert the OCaml list [a] to a JavaScript
+    array, using the provided function [conv] to convert array items. *)
+
+val list_of_js : ('a -> 'b) -> 'a Js.js_array Js.t -> 'b list
+(** [array_of_js conv a] convert the JavaScript array [a] to an OCaml
+    list, using the provided function [conv] to convert array items. *)
+
+
+(** {6 Javascript Introspection} *)
+
+val keys : 'a Js.t -> string list
+(** Return the attribute list of a JavaScript object. *)
+
+(** {6 Debugging JavaScript} *)
+
+val stringify : 'a -> string
+(** Stringify any value using JSON.  This is intended as a cheap
+    debugging utility function rather than a serialisation
+    facility. *)
+
+val debug : string -> ('a, unit, string, unit) format4 -> 'a
+(** [debug location fmt] report debugging information in a printf-style. *)
